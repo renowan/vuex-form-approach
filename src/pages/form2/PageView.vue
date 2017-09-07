48c4455b49726bd88db0e8855e4b58c5ffd16bca
温 穎峰 <on@gnavi.co.jp>
2017-09-07 15:07:38 +0900

    項目フォーム追加、リセット機能追加

--- src/pages/form2/PageView.vue
+++ src/pages/form2/PageView.vue
@@ -3,37 +3,79 @@
 
     <div class="row">
       <div class="col-xs-12 mb24">
-        複数のフォームのmodelをlocal stateに詰め込んで、発火するたびにstoreへどん
+        複数のバリデーションありのフォームと戦う。入力中の状態、バリデーション設定はすべてStoreに置く方針で実装する。
       </div>
     </div>
 
     <div class="row">
       <div class="col-xs-6">
 
-        <h3>Profile</h3>
+
 
         <div class="row">
           <div class="col-xs-12">
 
-            <div class="form-group">
-              <validation :order="myStore.profile.name">
-                <label>名前</label>
-                <input type="text" class="form-control input-sm" placeholder="" :value="myStore.profile.name.value" @input="inputUpdate($event.target.value, 'profile.name')">
-              </validation>
-            </div>
-            <div class="form-group">
-              <validation :validation="myStore.profile.age">
-                <label>年齢</label>
-                <input type="text" class="form-control input-sm" placeholder="" :value="myStore.profile.age.value" @input="inputUpdate($event.target.value, 'profile.age')">
-              </validation>
-            </div>
+            <h3>Profile</h3>
+
+            <validation :order="myStore.profile.name">
+              <label>名前</label>
+              <input
+              type="text"
+              class="form-control input-sm"
+              placeholder=""
+              :value="myStore.profile.name.value"
+              @input="inputUpdate($event.target.value, 'profile.name')">
+            </validation>
+
+            <validation :order="myStore.profile.age">
+              <label>年齢</label>
+              <input
+              type="text"
+              class="form-control input-sm"
+              placeholder=""
+              :value="myStore.profile.age.value"
+              @input="inputUpdate($event.target.value, 'profile.age')">
+            </validation>
+
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <h3>Info</h3>
+
+            <validation :order="myStore.info.graduated">
+              <label>出身校</label>
+              <input
+              type="text"
+              class="form-control input-sm"
+              placeholder=""
+              :value="myStore.info.graduated.value"
+              @input="inputUpdate($event.target.value, 'info.graduated')">
+            </validation>
+
+            <validation :order="myStore.info.phoneNum">
+              <label>電話番号（ハイフン無し）</label>
+              <input
+              type="text"
+              class="form-control input-sm"
+              placeholder=""
+              :value="myStore.info.phoneNum.value"
+              @input="inputUpdate($event.target.value, 'info.phoneNum')">
+            </validation>
+
           </div>
         </div>
 
 
         <div class="row">
           <div class="col-xs-12">
-
+            <button type="button" class="btn btn-default btn-sm" @click="reset">
+              リセット
+            </button>
+            <button type="button" class="btn btn-default btn-primary btn-sm" :disabled="myStore.disabled">
+              保存
+            </button>
           </div>
         </div>
 
@@ -63,27 +105,15 @@ export default {
       return this.$store.state.form2
     }
   },
-  data () {
-    return {
-    }
-  },
-  watch: {
-    // 'profile': {
-    //   handler (val) {
-    //     this.$store.commit('FORM1_UPDATE_PROFILE', cloneDeep(val))
-    //   },
-    //   deep: true
-    // }
-  },
-  filters: {
-    pretty (val) {
-      return JSON.stringify(JSON.parse(val), null, 2)
-    }
+  created () {
+    this.$store.dispatch('form2/checkError')
   },
   methods: {
     inputUpdate (value, path) {
-      // console.log('key', key)
       this.$store.dispatch('form2/inputUpdate', {value, path})
+    },
+    reset () {
+      this.$store.commit('form2/RESET_INPUT_DATA')
     }
   }
 }
@@ -92,5 +122,8 @@ export default {
 <style scoped>
 .form-group label {
   font-size: 12px;
+}
+pre {
+  font-size: 11px;
 }
 </style>
