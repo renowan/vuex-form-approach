ad5600cf65b9ea9b3d474745d9d8c8caf269a15a
温 穎峰 <on@gnavi.co.jp>
2017-09-07 15:06:45 +0900

    項目追加、入力せずに全体チェックアクション追加

--- src/store/modules/form2.js
+++ src/store/modules/form2.js
@@ -1,6 +1,30 @@
 import {cloneDeep} from 'lodash'
 import validationCheck from './validationCheck.js'
-const state = {
+
+/*
+  [データフォーマット]
+  {
+    value: '',           // [String] 値
+    validation: {        // [Object] バリデーション情報
+      rules: [],               // [Array]   バリデーションrule設定
+      isError: true,           // [Boolean] バリデーション結果
+      errors: [],              // [Array]   バリデーションエラーのメッセージ（複数）
+    },
+    type: '',            // [String]  型
+    skipFirstTime: true, // [Boolean] 初回バリデーションチェックするか
+    dirty: true,         // [Boolean] 情報は一同入力されたか
+  }
+
+  [rules]
+  required : 入力必須
+  spaceOnly: スペース、改行のみは不可
+  min      : 最小文字数（numberの場合は桁）
+  max      : 最大文字数（numberの場合は桁）
+  minValue : 最小値（numberで評価）
+  maxValue : 最大値（numberで評価）
+  isNumber : 半角数字のみ
+ */
+const defaultState = {
   profile: {
     name: {
       value: '',
@@ -15,23 +39,60 @@ const state = {
         formClass: ''
       },
       type: 'string',
-      skipFirstTime: false,
+      skipFirstTime: true,
       dirty: false, // 一度でも入力されたか
     },
     age: {
-      value: 1,
+      value: 200,
       validation: {
-        rulues: [
-
+        rules: [
+          'required',
+          'isNumber',
+          {max: 3},
+          {minValue: 1},
+          {maxValue: 150}
         ],
         isError: false, //バリデーション結果
         errors: [],
         formClass: ''
       },
       type: 'number',
-      skipFirstTime: true,
+      skipFirstTime: false,
       dirty: false, // 一度でも入力されたか
     },
+  },
+  info: {
+    graduated: {
+      value: '',
+      validation: {
+        rules: [
+          'spaceOnly',
+          {max: 10},
+        ],
+        isError: false, //バリデーション結果
+        errors: [],
+        formClass: ''
+      },
+      type: 'string',
+      skipFirstTime: false,
+      dirty: false, // 一度でも入力されたか
+    },
+    phoneNum: {
+      value: '',
+      validation: {
+        rules: [
+          'spaceOnly',
+          'isNumber',
+          {max: 11},
+        ],
+        isError: false, //バリデーション結果
+        errors: [],
+        formClass: ''
+      },
+      type: 'number',
+      skipFirstTime: false,
+      dirty: false, // 一度でも入力されたか
+    }
   },
   disabled: false
 }
@@ -42,8 +103,9 @@ const getters = {
 }
 
 // 情報の更新
-const FORM2_INPUT_UPDATE = 'FORM2_INPUT_UPDATE'
-const FORM2_UPDATE_DISABLED = 'FORM2_UPDATE_DISABLED'
+const INPUT_UPDATE = 'INPUT_UPDATE'
+const UPDATE_DISABLED = 'UPDATE_DISABLED'
+const RESET_INPUT_DATA = 'RESET_INPUT_DATA'
 
 const checkError = state => {
   for (var variable in state) {
@@ -51,7 +113,6 @@ const checkError = state => {
       let child = state[variable]
       for (var variable2 in child) {
         if (child.hasOwnProperty(variable2)) {
-          console.log('child[variable2]',child[variable2])
           if (child[variable2].hasOwnProperty('validation') && child[variable2].validation.isError) {
             return true
           }
@@ -71,28 +132,50 @@ const actions = {
     let targetObj = localState[segments[0]][segments[1]]
     targetObj.value = data.value
     targetObj = validationCheck(targetObj)
-    commit(FORM2_INPUT_UPDATE, {obj: targetObj, path})
+    commit(INPUT_UPDATE, {obj: targetObj, path})
 
     let disabled = checkError(localState)
-    // check
 
-    commit(FORM2_UPDATE_DISABLED, disabled)
+    commit(UPDATE_DISABLED, disabled)
+  },
+  // 全体バリデーションエラーチェック
+  checkError ({commit, state}) {
+    let localState = cloneDeep(state)
+    for (var variable in localState) {
+      if (localState.hasOwnProperty(variable)) {
+        let child = localState[variable]
+        for (var variable2 in child) {
+
+          if (child.hasOwnProperty(variable2)) {
+            if (child[variable2].hasOwnProperty('validation')) {
+              const targetObj = child[variable2]
+              child[variable2] = validationCheck(targetObj)
+              commit(INPUT_UPDATE, {obj: targetObj, path: `${variable}.${variable2}`})
+            }
+          }
+        }
+      }
+    }
+    commit(UPDATE_DISABLED, checkError(localState))
   }
 }
 
 // mutations
 const mutations = {
-  [FORM2_INPUT_UPDATE] (state, data) {
-    const segments = data.path.split('.')
-    state[segments[0]][segments[1]] = data.obj
+  [INPUT_UPDATE] (state, payload) {
+    const segments = payload.path.split('.')
+    state[segments[0]][segments[1]] = payload.obj
   },
-  [FORM2_UPDATE_DISABLED] (state, disabled) {
-    state.disabled = disabled
+  [UPDATE_DISABLED] (state, payload) {
+    state.disabled = payload
+  },
+  [RESET_INPUT_DATA] (state) {
+    Object.assign(state, defaultState)
   }
 }
 
 export default {
-  state,
+  state: JSON.parse(JSON.stringify(defaultState)),
   getters,
   actions,
   mutations,
